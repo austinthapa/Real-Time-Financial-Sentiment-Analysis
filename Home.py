c8dee4cb50c4516856003882d44e4d656fc009d9
Anil Thapa <anilthapa1.at23@gmail.com>
2025-04-29 22:57:07 -0500

    feat: add streamlit pages

---
+++ Home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title='Financial Sentiment Analysis', layout='wide')
+
+st.header('Main Dashboard for Financial Sentiment')
+st.subheader('Discuss the sentiment of different sectors')
